b3f44f20bd9cc7c4a38733d1e42f4c06e86ab05a
Gizem Kaya <76160232+gizem-ky@users.noreply.github.com>
2023-10-31 16:11:29 +0300

    authentication

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <div>
-      this is a protected route
+      <UserButton
+        afterSignOutUrl="/"
+      />
     </div>
   )
 }

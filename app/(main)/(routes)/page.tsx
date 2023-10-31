92126d8cd5c005941ce9ee361a84bbed3d259b79
Gizem Kaya <76160232+gizem-ky@users.noreply.github.com>
2023-10-31 15:00:39 +0300

    folders setup

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -4,12 +4,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <p className='text-3xl font-bold text-indigo-500'>
-      hello discord clone
-    </p>
-    <Button>
-      click me
-    </Button>
+      this is a protected route
     </div>
   )
 }

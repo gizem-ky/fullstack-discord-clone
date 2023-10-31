9201b0ccbc298e056cd5b7f4ee3480b7135efcde
Gizem Kaya <76160232+gizem-ky@users.noreply.github.com>
2023-10-31 13:35:25 +0300

    environment setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <p className='text-3xl font-bold text-indigo-500'>
+    <div>
+      <p className='text-3xl font-bold text-indigo-500'>
       hello discord clone
     </p>
+    <Button>
+      click me
+    </Button>
+    </div>
   )
 }

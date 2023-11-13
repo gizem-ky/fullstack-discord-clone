4c192c46b7d0cb16a409c07af3d48e82e5b7c054
Gizem Kaya <76160232+gizem-ky@users.noreply.github.com>
2023-11-13 14:11:38 +0300

    delete & leave server modal

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -7,6 +7,8 @@ import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { MembersModal } from "@/components/modals/members-modal";
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
+import { LeaveServerModal } from "@/components/modals/leave-server-modal";
+import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 
 
 export const ModalProvider = () => {
@@ -27,6 +29,8 @@ export const ModalProvider = () => {
       <EditServerModal />
       <MembersModal/>
       <CreateChannelModal/>
+      <LeaveServerModal/>
+      <DeleteServerModal/>
     </>
   )
 }

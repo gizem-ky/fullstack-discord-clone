f8612c9d5f593e83f0165f7654bf694d221c560f
Gizem Kaya <76160232+gizem-ky@users.noreply.github.com>
2023-11-23 13:35:46 +0300

    delete message

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,9 +2,9 @@
 
 import { useEffect, useState } from "react";
 
+import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
-import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { MembersModal } from "@/components/modals/members-modal";
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 import { LeaveServerModal } from "@/components/modals/leave-server-modal";
@@ -12,7 +12,7 @@ import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
 import { EditChannelModal } from "@/components/modals/edit-channel-modal";
 import { MessageFileModal } from "@/components/modals/message-file-modal";
-
+import { DeleteMessageModal } from "@/components/modals/delete-message-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -30,13 +30,14 @@ export const ModalProvider = () => {
       <CreateServerModal />
       <InviteModal />
       <EditServerModal />
-      <MembersModal/>
-      <CreateChannelModal/>
-      <LeaveServerModal/>
-      <DeleteServerModal/>
-      <DeleteChannelModal/>
-      <EditChannelModal/>
-      <MessageFileModal/>
+      <MembersModal />
+      <CreateChannelModal />
+      <LeaveServerModal />
+      <DeleteServerModal />
+      <DeleteChannelModal />
+      <EditChannelModal />
+      <MessageFileModal />
+      <DeleteMessageModal />
     </>
   )
 }

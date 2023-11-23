bd49c921606a92aae2dfed1c4b20878cbc57c611
Gizem Kaya <76160232+gizem-ky@users.noreply.github.com>
2023-11-23 11:06:49 +0300

    chat input component

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -4,6 +4,7 @@ import { ChannelType } from "@prisma/client";
 
 import { currentProfile } from "@/lib/current-profile";
 import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
 import { db } from "@/lib/db";
 
 interface ChannelIdPageProps {
@@ -46,7 +47,19 @@ const ChannelIdPage = async ({
         serverId={channel.serverId}
         type="channel"
       />
-      
+      {channel.type === ChannelType.TEXT && (
+        <>
+          <ChatInput
+            name={channel.name}
+            type="channel"
+            apiUrl="/api/socket/messages"
+            query={{
+              channelId: channel.id,
+              serverId: channel.serverId,
+            }}
+          />
+        </>
+      )}
     </div>
    );
 }

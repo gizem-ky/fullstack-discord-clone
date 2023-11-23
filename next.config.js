6ababad4fda55bec9ad97b2ffa9a13cfa836fbcc
Gizem Kaya <76160232+gizem-ky@users.noreply.github.com>
2023-11-23 13:47:00 +0300

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
-    images: {
-      domains: [
-        "uploadthing.com",
-        "utfs.io"
-      ]
-    }
+  webpack: (config) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil"
+    });
+
+    return config;
+  },
+  images: {
+    domains: [
+      "uploadthing.com",
+      "utfs.io"
+    ]
   }
-  
-  module.exports = nextConfig
+}
+
+module.exports = nextConfig

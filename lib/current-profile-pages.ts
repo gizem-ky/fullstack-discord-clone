e628c6fddf1ed682742777c387229def05d3c41b
Gizem Kaya <76160232+gizem-ky@users.noreply.github.com>
2023-11-23 11:20:27 +0300

    messages api

--- lib/current-profile-pages.ts
+++ lib/current-profile-pages.ts
@@ -1,8 +1,9 @@
-import { NextApiRequest  } from "next";
+import { NextApiRequest } from "next";
 import { getAuth } from "@clerk/nextjs/server";
+
 import { db } from "@/lib/db";
 
-export const currentProfile = async (req: NextApiRequest) => {
+export const currentProfilePages = async (req: NextApiRequest) => {
   const { userId } = getAuth(req);
 
   if (!userId) {
